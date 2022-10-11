980a6b51e885983331bf5aeebbbba202f12a4039
paasong <happy001020@naver.com>
2022-10-11 19:01:45 +0900

    피보나치수열

---
+++ Programmers/Level 2/피보나치수열.py
@@ -0,0 +1,13 @@
+def solution(n):
+    num1 = 0
+    num2 = 1
+    for _ in range(n-1):
+        temp = num2
+        num2 = num1 + num2
+        num1 = temp
+
+        
+
+    return num2 % 123456
+
+print(solution(5))
