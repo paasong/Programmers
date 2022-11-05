bf7cb62f5111a501f3a54d604d89770ec43a9d56
paasong <happy001020@naver.com>
2022-11-05 21:07:53 +0900

    점프와 순간이동

--- Programmers/Level 2/점프와순간이동.py
+++ Programmers/Level 2/점프와순간이동.py
@@ -1,24 +1,30 @@
+# def solution(n):
+#     ans = 0
+#     n = n
+#
+#     return cnt(n, ans)
+#
+# def cnt(n, ans):
+#     if n == 1:
+#         ans += 1
+#         return ans
+#
+#     if n % 2 == 1:
+#         ans += 1
+#         n = n // 2
+#         return cnt(n, ans)
+#     else:
+#         n = n // 2
+#         return cnt(n, ans)
+#
+
 def solution(n):
-    ans = 0
-    n = n
-
-    return cnt(n, ans)
-
-
-def cnt(n, ans):
-    if n == 1:
-        ans += 1
-        return ans
-
-    if n % 2 == 1:
-        ans += 1
+    ans = 1
+    while n > 1:
+        ans += n % 2
         n = n // 2
-        return cnt(n, ans)
-    else:
-        n = n // 2
-        return cnt(n, ans)
+    return ans
 
 
-
-print( solution(6))
+print(solution(6))
 
