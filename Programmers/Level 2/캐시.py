736045d1005bcba990b965f03a945cd4ab0f0d3f
paasong <happy001020@naver.com>
2023-02-11 01:21:53 +0900

    캐시

--- Programmers/Level 2/캐시.py
+++ Programmers/Level 2/캐시.py
@@ -1,20 +1,39 @@
+# def solution(cacheSize, cities):
+#     cache = list()
+#     answer = 0
+#     while cities:
+#         city = cities.pop(0)
+#         city = city.lower()
+#
+#
+#         if city in cache:
+#             answer += 1
+#             cache.remove(cache.index(city))
+#            #cache.remove(city)
+#         else:
+#             answer += 5
+#         cache.append(city)
+#         if len(cache) > cacheSize:
+#             cache.pop(0)
+#
+#     return answer
+
 def solution(cacheSize, cities):
-    cache = list()
-    answer = 0
-    while cities:
-        city = cities.pop(0)
-        city = city.lower()
+    import collections
+    cache = collections.deque(maxlen=cacheSize)
+    print(cache)
+    time = 0
 
-
-        if city in cache:
-            answer += 1
-            cache.pop(cache.index(city))
+    for i in cities:
+        s = i.lower()
+        if s in cache:
+            cache.remove(s)
+            cache.append(s)
+            time += 1
         else:
-            answer += 5
-        cache.append(city)
-        if len(cache) > cacheSize:
-            cache.pop(0)
+            cache.append(s)
+            time += 5
+    return time
 
-    return answer
 
 print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
